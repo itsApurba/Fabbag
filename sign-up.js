2432250e607244b28d46a696c6a33566bab2f263
Tridip Rong <59462773+TridipRong@users.noreply.github.com>
2022-05-05 22:28:08 +0530

    sign-up created

--- sign-up.js
+++ sign-up.js
@@ -1,16 +1,16 @@
-document.querySelector("#masaiForm").addEventListener("submit",addMatch);
-var personArr=JSON.parse(localStorage.getItem("schedule")) || []
-function addMatch(){
+document.querySelector("#form").addEventListener("submit",addMatch);
+var personArr=JSON.parse(localStorage.getItem("personData")) || []
+function addPerson(){
     event.preventDefault();
-    var matchObj={
+    var personObj={
         firstName:form.first_name.value,
         lastName:form.last_name.value,
         email:form.email.value,
         password:form.Password.value,
        
     }
-    matchArr.push(matchObj);
-    localStorage.setItem("schedule",JSON.stringify(matchArr))
+    personArr.push(personObj);
+    localStorage.setItem("personData",JSON.stringify(matchArr))
     
-    window.location.href="./matches.html";
+    // window.location.href="./matches.html";
 }
