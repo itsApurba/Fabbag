cfbf2cc85770f8977ab6e1cdd6c2b34e10563486
Tridip Rong <59462773+TridipRong@users.noreply.github.com>
2022-05-06 01:10:17 +0530

    sign-up added

--- sign-up.js
+++ sign-up.js
@@ -3,14 +3,15 @@ var personArr=JSON.parse(localStorage.getItem("personData")) || []
 function addPerson(){
     event.preventDefault();
     var personObj={
-        firstName:form.first_name.value,
+        firstName:form.first_name.value,     
         lastName:form.last_name.value,
         email:form.email.value,
         password:form.Password.value,
        
     }
+    console.log(personObj)
     personArr.push(personObj);
     localStorage.setItem("personData",JSON.stringify(personArr))
-    
+    console.log(personArr)
     // window.location.href="./matches.html";
 }
