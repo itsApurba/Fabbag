214765d982a7fb242077c77944bb185cb82c6130
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2022-05-06 19:03:03 +0000

    ++

---
+++ product.js
@@ -0,0 +1,40 @@
+// console.log("hi");
+
+const tabs = document.querySelectorAll("#tabs>li>button");
+tabs.forEach((el) => {
+  el.addEventListener(
+    "click",
+    () => {
+      // console.log(el.innerText);
+      // if(el.innerText == ){
+      console.log(el.classList);
+    }
+    // el.toggleAttribute("active");
+  );
+});
+
+// create a click to open tab layout javascript
+
+window.addEventListener("load", function () {
+  // store tabs variable
+  var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+  function myTabClicks(tabClickEvent) {
+    for (var i = 0; i < myTabs.length; i++) {
+      myTabs[i].classList.remove("active");
+    }
+    var clickedTab = tabClickEvent.currentTarget;
+    clickedTab.classList.add("active");
+    tabClickEvent.preventDefault();
+    var myContentPanes = document.querySelectorAll(".tab-pane");
+    for (i = 0; i < myContentPanes.length; i++) {
+      myContentPanes[i].classList.remove("active");
+    }
+    var anchorReference = tabClickEvent.target;
+    var activePaneId = anchorReference.getAttribute("href");
+    var activePane = document.querySelector(activePaneId);
+    activePane.classList.add("active");
+  }
+  for (i = 0; i < myTabs.length; i++) {
+    myTabs[i].addEventListener("click", myTabClicks);
+  }
+});
