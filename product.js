8c86115994381987632ef2e3ae6c620dc428106a
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2022-05-07 09:10:07 +0000

    ++

--- product.js
+++ product.js
@@ -1,35 +1,45 @@
-// console.log("hi");
+//
 
 const tabs = document.querySelectorAll("#tabs>li>button");
-tabs.forEach((el) => {
-  el.addEventListener(
-    "click",
-    () => {
-      // console.log(el.innerText);
-      // if(el.innerText == ){
-      console.log(el.classList);
-    }
-    // el.toggleAttribute("active");
-  );
-});
+// tabs.forEach((el) => {
+//   el.addEventListener(
+//     "click",
+//     () => {
+//       //
+//       // if(el.innerText == ){
+//     }
+//     // el.toggleAttribute("active");
+//   );
+// });
 
 // create a click to open tab layout javascript
 
 window.addEventListener("load", function () {
   // store tabs variable
-  var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+  var myTabs = document.querySelectorAll("#tabs li button");
+
+  tabs.forEach((el) => {
+    el.addEventListener("click", myTabClicks);
+  });
+
   function myTabClicks(tabClickEvent) {
     for (var i = 0; i < myTabs.length; i++) {
       myTabs[i].classList.remove("active");
     }
     var clickedTab = tabClickEvent.currentTarget;
+
     clickedTab.classList.add("active");
     tabClickEvent.preventDefault();
-    var myContentPanes = document.querySelectorAll(".tab-pane");
+
+    var myContentPanes = document.querySelectorAll("#tabContents>div");
+    console.log("myContentPanes: ", myContentPanes);
+
     for (i = 0; i < myContentPanes.length; i++) {
       myContentPanes[i].classList.remove("active");
     }
+
     var anchorReference = tabClickEvent.target;
+    console.log("anchorReference: ", anchorReference);
     var activePaneId = anchorReference.getAttribute("href");
     var activePane = document.querySelector(activePaneId);
     activePane.classList.add("active");
