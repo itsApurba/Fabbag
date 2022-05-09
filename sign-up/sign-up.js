dea19b0ba2a1aed2d2a33dc52f165b0fc9dbb0c9
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2022-05-09 14:59:14 +0000

    ++

--- sign-up/sign-up.js
+++ sign-up/sign-up.js
@@ -1,16 +1,16 @@
-document.querySelector("#form").addEventListener("submit",addPerson);
-var personArr=JSON.parse(localStorage.getItem("personData")) || [];
-function addPerson(){
-    event.preventDefault();
-    var personObj={
-        fistName:form.firstName.value,
-        lastName:form.lastName.value,
-        email:form.email.value,
-        password:form.password.value,
-    }
-    personArr.push(personObj);
-    localStorage.setItem("personData",JSON.stringify(personArr))
-    alert("Congratulation\ Your Account Is Created Successfully");
-   
-    form.reset()
+document.querySelector("#form").addEventListener("submit", addPerson);
+var personArr = JSON.parse(localStorage.getItem("personData")) || [];
+function addPerson() {
+  event.preventDefault();
+  var personObj = {
+    fistName: form.firstName.value,
+    lastName: form.lastName.value,
+    email: form.email.value,
+    password: form.password.value,
+  };
+  personArr.push(personObj);
+  localStorage.setItem("personData", JSON.stringify(personArr));
+  alert("Congratulation Your Account Is Created Successfully");
+
+  window.location.href = "../login/signIn.html";
 }
