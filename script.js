5ef591f57c70e388e0b21bd98889db6cd76dce83
anuragg0107 <anurag.gupta199418@gmail.com>
2022-05-09 15:47:24 +0530

    fab bag homepage

--- script.js
+++ script.js
@@ -58,24 +58,9 @@ function slideshow(num){
     slides[num].style.display="block";
 
 }
-// const productContainers = [...document.querySelectorAll('.product-container')];
-// const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-// const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-// productContainers.forEach((item, i) => {
-//     let containerDimensions = item.getBoundingClientRect();
-//     let containerWidth = containerDimensions.width;
 
-//     nxtBtn[i].addEventListener('click', () => {
-//         item.scrollLeft += containerWidth;
-//     })
-
-//     preBtn[i].addEventListener('click', () => {
-//         item.scrollLeft -= containerWidth;
-//     })
-// })
-
-// what's in the bag java
+// what's in the bag js
 
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
